f3739324f6b3b1a32dce2a32e8b3695a9645a4f7
Luca Ferrario <ferrariolucait@gmail.com>
2022-02-25 13:50:03 +0100

    fb

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB98n99ru9pqBIokhNyWy8Xk3tBB-KY5o0",
+  authDomain: "nite-mobile-396a5.firebaseapp.com",
+  projectId: "nite-mobile-396a5",
+  storageBucket: "nite-mobile-396a5.appspot.com",
+  messagingSenderId: "463647968516",
+  appId: "1:463647968516:web:7c3bf140c1c52b9905d451",
+};
