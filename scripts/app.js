c3c516ca34e4a9a25aa6fadc16c62c862911b8bd
Luca Ferrario <ferrariolucait@gmail.com>
2022-06-13 17:51:24 +0200

    mobile nav

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,11 @@ function sendMail() {
     encodeURIComponent(document.getElementById("msgtxt").value);
   window.location.href = link;
 }
+
+const hamburger = document.querySelector(".hamburger");
+const mobilenav = document.querySelector(".mobile-nav");
+
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+  mobilenav.classList.toggle("is-active");
+});
